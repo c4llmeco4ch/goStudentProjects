a64cbdf16732b8fabd76a33d406ecc40f03fe23c
c4llmeco4ch <cbb692@gmail.com>
2019-10-24 15:47:04 -0700

    Completed binarysearch code

--- binarySearch/binarysearch.go
+++ binarySearch/binarysearch.go
@@ -1,36 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
 
 var (
-	guess int8
-	low   int8
-	high  int8
+	guess int32
+	low   int32
+	high  int32
 )
 
 func main() {
-	low = 0
-	high = 100
 	arePlaying := true
 	for arePlaying {
 		var validGame = performSearch()
 		if !validGame {
 			arePlaying = false
 			fmt.Println("We don't play with cheaters. Goodbye >:(")
-		}
-		else{
-			//TODO: take in user input to play again
+		} else {
+			fmt.Print("Would you like to play again? ('y'/'n') ")
+			arePlaying = getResponse() == 'y'
 		}
 	}
 }
 
+func getResponse() rune {
+	reader := bufio.NewReader(os.Stdin)
+	response, _, err := reader.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+	}
+	response = unicode.ToLower(response)
+	return response
+}
+
 func performSearch() bool {
+	low = 0
+	high = 100
 	areCorrect := false
-	fmt.Printf("Let's begin. Please choose a number between %d and %d", low, high)
+	fmt.Printf("Let's begin. Please choose a number between %d and %d...\n", low, high)
 	for !areCorrect && low <= high {
-		guess = int8((high + low) / 2)
-		fmt.Printf("My guess is %d.\nIs this 'c'orrect, too 'h', or too 'l'ow", guess)
-		//TODO: take in user input to determine guess validity
+		guess = int32((high + low) / 2)
+		fmt.Printf("My guess is %d.\nIs this 'c'orrect, too 'h', or too 'l'ow: ", guess)
+		var response rune
+		isValid := false
+		for !isValid {
+			response = getResponse()
+			if response == 'c' {
+				fmt.Println("Awesome! Good game :)")
+				return true
+			} else if response == 'h' {
+				high = guess - 1
+				isValid = true
+			} else if response == 'l' {
+				low = guess + 1
+				isValid = true
+			} else {
+				fmt.Print("This is an invalid reponse. Please try 'c', 'h', or 'l': ")
+			}
+		}
 	}
 	return areCorrect
 }
